2cae297ea7012caa06f39e0e4c1ca105795f5f72
Jason Kim <yong076@gmail.com>
2015-01-07 20:15:03 +0900

    Second Change

--- app/build/generated/source/r/debug/com/samir/R.java
+++ app/build/generated/source/r/debug/com/samir/R.java
@@ -38,14 +38,16 @@ public final class R {
     }
     public static final class layout {
         public static final int detail=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int option=0x7f030002;
-        public static final int row=0x7f030003;
+        public static final int fragment_fragment_one=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int option=0x7f030003;
+        public static final int row=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main_actions=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int hello_blank_fragment=0x7f050001;
     }
 }
